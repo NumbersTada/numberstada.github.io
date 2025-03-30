e74ef3f145fd2dfb694e8e9df4a02b602a2cf4ff
NumbersTada <43664372+NumbersTada@users.noreply.github.com>
2025-03-30 19:13:54 +0200

    Create algorithms.js

---
+++ 2048/v/auto/auto-move/algorithms.js
@@ -0,0 +1,19 @@
+[
+{name: "Random Keys", author: "pluh", obj: {
+  getBestMove: function () {
+    return Math.floor(Math.random() * 4)
+  }
+}},
+{name: "Random 3 Keys", author: "pluh", obj: {
+  getBestMove: function () {
+    return Math.floor(Math.random() * 3)
+  }
+}},
+{name: "Swirl", author: "Mangos", obj: {
+  cycle: 0,
+  getBestMove: function () {
+    ++this.cycle;
+    this.cycle %= 4;
+    return this.cycle;
+  }
+]
